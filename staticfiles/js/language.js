b00e53399243dc0a527eb5c573b64a6d1f299655
d0rian3 <ggproebali16.0@gmail.com>
2025-09-05 20:27:24 +0200

    lala

---
+++ staticfiles/js/language.js
@@ -0,0 +1,307 @@
+export const language = {
+  ru: {
+    navOffer: "Что мы предлагаем",
+    navAdvantages: "Преимущества",
+    navShop: "Магазин",
+    navFaq: "Вопросы",
+    navGallery: "Галерея",
+    navContact: "Свяжитесь с нами",
+
+    headerTitle: "Зарабатывай более 3000 Kč в день работая с нами и получи ряд преимуществ",
+    headerSubtitle: "Ищете надежный электровелосипед и флотилию для работы курьером? Мы здесь для вас! Предлагаем самые комфортные, безопасные электровелосипеды FATBIKE",
+    headerButton: "🚀 Свяжитесь с нами и начните зарабатывать",
+
+    servicesButton: "Сервисы",
+    servicesSubtitle: "Всё для вашего транспорта — надёжно, удобно, профессионально.\nОбслуживание, аренда, продажа и выкуп — всё в одном месте:",
+
+    servicesCard1Title: "Обслуживание транспорта",
+    servicesCard1Description: "Надёжный ремонт и сервис для вашего транспорта.",
+
+    servicesCard2Title: "Флотилия Болт",
+    servicesCard2Description: "Управление и обслуживание парков для курьеров и водителей.",
+
+    servicesCard3Title: "Аренда электровелосипедов",
+    servicesCard3Description: "Современные электровелосипеды в аренду на выгодных условиях.",
+
+    servicesCard4Title: "Продажа электровелосипедов",
+    servicesCard4Description: "Новые и проверенные электровелосипеды по доступным ценам.",
+
+    servicesCard5Title: "Аренда с выкупом",
+    servicesCard5Description: "Удобный способ приобрести транспорт — аренда с последующим выкупом.",
+
+    servicesCard6Title: "Ремонт и сервис",
+    servicesCard6Description: "Качественное техническое обслуживание и ремонт любого транспорта.",
+
+    caseStudiesTitle: "Давайте двигаться вперёд вместе",
+    caseStudiesButton: "Получить консультацию",
+        fatbikeAdvantagesTitle: "Курьеру в Праге будет удобнее ездить на FATBIKE по нескольким причинам:",
+    fatbikeAdvantages1: "Проходимость: колеса обеспечивают хорошее сцепление на неровных и скользких дорогах.",
+    fatbikeAdvantages2: "Устойчивость: Fat bike лучше для Праги, потому что его широкие шины смягчают брусчатку, справляются со снегом и позволяют комфортно ездить по паркам и бордюрам.",
+    fatbikeAdvantages3: "Зима и снег: Fat bike легко передвигается по снегу и льду, что важно зимой.",
+    fatbikeAdvantages4: "Комфорт: Fat bike поглощает удары, снижая вибрацию и усталость курьера.",
+    fatbikeAdvantages5: "Долговечность: Прочные шины уменьшают риск повреждений, сокращают количество ремонтов.",
+    fatbikeAdvantages6: "Таким образом, FATBIKE удобен для курьера, особенно в плохую погоду",
+
+    buybackConditionsTitle: "Предлагаем курьерам выгодные условия для ВЫКУПА и ПОКУПКИ электровелосипедов по доступным ценам. Наши предложения включают:",
+    buybackConditions1: "Льготные условия покупки/выкупа – курьеры могут приобрести новый электровелосипед с существенной скидкой",
+    buybackConditions2: "Программа лояльности – бонусы и скидки для курьеров, которые остаются с нами длительное время.",
+    buybackConditions3: "Скидки за объем заказов– для курьеров, имеющих большое количество выполненных заказов, мы предлагаем дополнительные скидки при покупке или выкупе электровелосипеда.",
+    buybackConditions4: "Эти предложения помогут курьерам работать эффективно и экономно!",
+
+    faqButton: "Часто задаваемые вопросы",
+
+    whatWeDoTitle: "Что мне нужно для работы?",
+    whatWeDoAge: "Возраст от 16 лет",
+    whatWeDoPhone: "Смартфон(ios, android) с доступом в интернет",
+    whatWeDoResidence: "Легальное пребывание в Чехии",
+
+    whatWeDoCommissionHeader: "Какой процент вы удерживаете?",
+    whatWeDoCommission: "Мы берем 700 крон от заработка, который вы видите в приложении",
+
+    whatWeDoPayoutHeader: "Когда вы отправляете выплату?",
+    whatWeDoPayout1: "Выплаты производятся каждый вторник(исключение - праздничные дни)",
+    whatWeDoPayout2: "После праздников в течении 1-2 рабочих дней 🤓☝🏼",
+
+    whatWeDoTransferHeader: "Можете ли вы перевести выплату на счет, который не принадлежит мне?",
+    whatWeDoTransfer: "Да. Например, счет вашего родственника или друга",
+
+    whatWeDoBenefitsHeader: "Какие преимущества работать курьером на электровелосипеде?",
+    whatWeDoBenefits: "Работа курьером на электровелосипеде FATBIKE дает преимущества: -высокая проходимость в любых условиях -комфорт на неровных дорогах -скорость и экономия сил за счет электропривода -независимость от пробок и затрат на топливо 🚴⚡",
+
+    captionHeader: "Аренда лучших электровелосипедов для доставки в Праге! 🚴‍♂️🚗",
+
+    contactUsButton: "Свяжитесь с нами",
+    allCategories: "Все",
+    legendAction: "Действия",
+    navCart: "Корзина",
+  },
+
+  uk: {
+    navOffer: "Що ми пропонуємо",
+    navAdvantages: "Переваги",
+    navShop: "Магазин",
+    navFaq: "Питання",
+    navGallery: "Галерея",
+    navContact: "Зв’язатися з нами",
+
+    headerTitle: "Заробляйте понад 3000 Kč на день, працюючи з нами, та отримайте низку переваг",
+    headerSubtitle: "Шукаєте надійний електровелосипед і флотилію для роботи кур'єром? Ми тут для вас! Пропонуємо найкомфортніші, безпечні електровелосипеди FATBIKE",
+    headerButton: "🚀 Зв’яжіться з нами та почніть заробляти",
+
+    servicesButton: "Сервіси",
+    servicesSubtitle: "Все для вашого транспорту — надійно, зручно, професійно.\nОбслуговування, оренда, продаж і викуп — все в одному місці:",
+
+    servicesCard1Title: "Обслуговування транспорту",
+    servicesCard1Description: "Надійний ремонт та сервіс для вашого транспорту.",
+
+    servicesCard2Title: "Флотилія Болт",
+    servicesCard2Description: "Управління та обслуговування парків для кур’єрів та водіїв.",
+
+    servicesCard3Title: "Оренда електровелосипедів",
+    servicesCard3Description: "Сучасні електровелосипеди в оренду на вигідних умовах.",
+
+    servicesCard4Title: "Продаж електровелосипедів",
+    servicesCard4Description: "Нові та перевірені електровелосипеди за доступними цінами.",
+
+    servicesCard5Title: "Оренда з викупом",
+    servicesCard5Description: "Зручний спосіб придбати транспорт — оренда з подальшим викупом.",
+
+    servicesCard6Title: "Ремонт і сервіс",
+    servicesCard6Description: "Якісне технічне обслуговування і ремонт будь-якого транспорту.",
+
+    caseStudiesTitle: "Рухаймося вперед разом",
+    caseStudiesButton: "Отримати консультацію",
+        fatbikeAdvantagesTitle: "Кур'єру в Празі буде зручніше їздити на FATBIKE з кількох причин:",
+    fatbikeAdvantages1: "Прохідність: колеса забезпечують гарне зчеплення на нерівних та слизьких дорогах.",
+    fatbikeAdvantages2: "Стійкість: Fat bike краще для Праги, оскільки його широкі шини пом'якшують бруківку, справляються зі снігом та дозволяють комфортно їздити по парках і бордюрах.",
+    fatbikeAdvantages3: "Зима і сніг: Fat bike легко рухається по снігу та льоду, що важливо взимку.",
+    fatbikeAdvantages4: "Комфорт: Fat bike поглинає удари, знижуючи вібрацію та втому кур'єра.",
+    fatbikeAdvantages5: "Довговічність: Міцні шини зменшують ризик пошкоджень і кількість ремонтів.",
+    fatbikeAdvantages6: "Таким чином, FATBIKE зручний для кур'єра, особливо в погану погоду",
+
+    buybackConditionsTitle: "Пропонуємо кур'єрам вигідні умови для ВИКУПУ та ПОКУПКИ електровелосипедів за доступними цінами. Наші пропозиції включають:",
+    buybackConditions1: "Пільгові умови покупки/викупу – кур'єри можуть придбати новий електровелосипед зі значною знижкою",
+    buybackConditions2: "Програма лояльності – бонуси та знижки для кур'єрів, які залишаються з нами надовго.",
+    buybackConditions3: "Знижки за обсяг замовлень – для кур'єрів з великою кількістю виконаних замовлень ми пропонуємо додаткові знижки при покупці або викупі електровелосипеда.",
+    buybackConditions4: "Ці пропозиції допоможуть кур'єрам працювати ефективно та економно!",
+
+    faqButton: "Часті запитання",
+
+    whatWeDoTitle: "Що мені потрібно для роботи?",
+    whatWeDoAge: "Вік від 16 років",
+    whatWeDoPhone: "Смартфон(iOS, Android) з доступом до інтернету",
+    whatWeDoResidence: "Легальне перебування в Чехії",
+
+    whatWeDoCommissionHeader: "Який відсоток ви утримуєте?",
+    whatWeDoCommission: "Ми беремо 700 крон від заробітку, який ви бачите в додатку",
+
+    whatWeDoPayoutHeader: "Коли ви відправляєте виплату?",
+    whatWeDoPayout1: "Виплати проводяться щовівторка (виняток — святкові дні)",
+    whatWeDoPayout2: "Після свят — протягом 1-2 робочих днів 🤓☝🏼",
+
+    whatWeDoTransferHeader: "Чи можете ви перевести виплату на рахунок, що не належить мені?",
+    whatWeDoTransfer: "Так. Наприклад, на рахунок родича або друга",
+
+    whatWeDoBenefitsHeader: "Які переваги роботи кур'єром на електровелосипеді?",
+    whatWeDoBenefits: "Робота кур'єром на електровелосипеді FATBIKE дає переваги: -висока прохідність у будь-яких умовах -комфорт на нерівних дорогах -швидкість і економія сил завдяки електроприводу -незалежність від заторів і витрат на паливо 🚴⚡",
+
+    captionHeader: "Оренда найкращих електровелосипедів для доставки у Празі! 🚴‍♂️🚗",
+
+    contactUsButton: "Зв’яжіться з нами",
+    allCategories: "Всi",
+    legendAction: "Що ми робимо",
+    navGallery: "Корзина",
+  },
+
+  en: {
+    navOffer: "What We Offer",
+    navAdvantages: "Advantages",
+    navShop: "Shop",
+    navFaq: "Questions",
+    navGallery: "Gallery",
+    navContact: "Contact Us",
+
+    headerTitle: "Earn over 3000 Kč per day working with us and get a range of benefits",
+    headerSubtitle: "Looking for a reliable electric bike and a fleet for courier work? We are here for you! Offering the most comfortable, safe FATBIKE electric bikes",
+    headerButton: "🚀 Contact us and start earning",
+
+    servicesButton: "Services",
+    servicesSubtitle: "Everything for your transport — reliable, convenient, professional.\nMaintenance, rental, sale, and buyout — all in one place:",
+
+    servicesCard1Title: "Transport Maintenance",
+    servicesCard1Description: "Reliable repair and service for your transport.",
+
+    servicesCard2Title: "Bolt Fleet",
+    servicesCard2Description: "Management and maintenance of parks for couriers and drivers.",
+
+    servicesCard3Title: "Electric Bike Rental",
+    servicesCard3Description: "Modern electric bikes for rent on favorable terms.",
+
+    servicesCard4Title: "Electric Bike Sales",
+    servicesCard4Description: "New and tested electric bikes at affordable prices.",
+
+    servicesCard5Title: "Rent-to-Own",
+    servicesCard5Description: "A convenient way to acquire transport — rental with subsequent buyout.",
+
+    servicesCard6Title: "Repair and Service",
+    servicesCard6Description: "High-quality technical maintenance and repair of any transport.",
+
+    caseStudiesTitle: "Let's move forward together",
+    caseStudiesButton: "Get a consultation",
+     fatbikeAdvantagesTitle: "It is more convenient for a courier in Prague to ride a FATBIKE for several reasons:",
+    fatbikeAdvantages1: "Off-road capability: the wheels provide good grip on uneven and slippery roads.",
+    fatbikeAdvantages2: "Stability: Fat bike is better for Prague because its wide tires soften cobblestones, handle snow, and allow comfortable riding through parks and curbs.",
+    fatbikeAdvantages3: "Winter and snow: Fat bike easily rides through snow and ice, which is important in winter.",
+    fatbikeAdvantages4: "Comfort: Fat bike absorbs shocks, reducing vibration and courier fatigue.",
+    fatbikeAdvantages5: "Durability: Strong tires reduce the risk of damage and minimize repairs.",
+    fatbikeAdvantages6: "Thus, FATBIKE is convenient for a courier, especially in bad weather",
+
+    buybackConditionsTitle: "We offer couriers favorable conditions for BUYBACK and PURCHASE of electric bikes at affordable prices. Our offers include:",
+    buybackConditions1: "Preferential purchase/buyback terms – couriers can purchase a new electric bike at a significant discount",
+    buybackConditions2: "Loyalty program – bonuses and discounts for couriers who stay with us for a long time.",
+    buybackConditions3: "Order volume discounts – for couriers with a large number of completed orders, we offer additional discounts when buying or buying out an electric bike.",
+    buybackConditions4: "These offers will help couriers work efficiently and economically!",
+
+    faqButton: "Frequently Asked Questions",
+
+    whatWeDoTitle: "What do I need to work?",
+    whatWeDoAge: "Age from 16 years",
+    whatWeDoPhone: "Smartphone (iOS, Android) with internet access",
+    whatWeDoResidence: "Legal stay in the Czech Republic",
+
+    whatWeDoCommissionHeader: "What percentage do you withhold?",
+    whatWeDoCommission: "We take 700 CZK from the earnings you see in the app",
+
+    whatWeDoPayoutHeader: "When do you send the payment?",
+    whatWeDoPayout1: "Payments are made every Tuesday (except public holidays)",
+    whatWeDoPayout2: "After holidays within 1-2 business days 🤓☝🏼",
+
+    whatWeDoTransferHeader: "Can you send payment to an account that does not belong to me?",
+    whatWeDoTransfer: "Yes. For example, to your relative's or friend's account",
+
+    whatWeDoBenefitsHeader: "What are the advantages of working as a courier on an electric bike?",
+    whatWeDoBenefits: "Working as a courier on a FATBIKE electric bike has advantages: -high cross-country ability in any conditions -comfort on uneven roads -speed and energy saving thanks to electric drive -independence from traffic jams and fuel costs 🚴⚡",
+
+    captionHeader: "Rental of the best electric bikes for delivery in Prague! 🚴‍♂️🚗",
+
+    contactUsButton: "Contact Us",
+    allCategories: "All",
+    legendAction: "What We Do",
+    navCart: "Cart",
+  },
+
+  cz: {
+    navOffer: "Co nabízíme",
+    navAdvantages: "Výhody",
+    navShop: "Obchod",
+    navFaq: "Otázky",
+    navGallery: "Galerie",
+    navContact: "Kontaktujte nás",
+
+    headerTitle: "Vydělávejte více než 3000 Kč denně prací s námi a získejte řadu výhod",
+    headerSubtitle: "Hledáte spolehlivé elektrické kolo a flotilu pro práci kurýra? Jsme tu pro vás! Nabízíme nejpohodlnější, bezpečná elektrická kola FATBIKE",
+    headerButton: "🚀 Kontaktujte nás a začněte vydělávat",
+
+    servicesButton: "Služby",
+    servicesSubtitle: "Vše pro vaši dopravu — spolehlivě, pohodlně, profesionálně.\nServis, pronájem, prodej a odkup — vše na jednom místě:",
+    servicesCard1Title: "Údržba dopravy",
+    servicesCard1Description: "Spolehlavá oprava a servis pro vaši dopravu.",
+
+    servicesCard2Title: "Flotila Bolt",
+    servicesCard2Description: "Správa a údržba parků pro kurýry a řidiče.",
+
+    servicesCard3Title: "Pronájem elektrických kol",
+    servicesCard3Description: "Moderní elektrická kola k pronájmu za výhodných podmínek.",
+
+    servicesCard4Title: "Prodej elektrických kol",
+    servicesCard4Description: "Nová a ověřená elektrická kola za dostupné ceny.",
+
+    servicesCard5Title: "Pronájem s odkoupem",
+    servicesCard5Description: "Pohodlný způsob, jak získat dopravu — pronájem s následným odkupem.",
+
+    servicesCard6Title: "Opravy a servis",
+    servicesCard6Description: "Kvalitní technická údržba a opravy jakékoliv dopravy.",
+    caseStudiesTitle: "Pojďme postupovat vpřed společně",
+    caseStudiesButton: "Získat konzultaci",
+    fatbikeAdvantagesTitle: "Pro kurýra v Praze je pohodlnější jezdit na FATBIKE z několika důvodů:",
+    fatbikeAdvantages1: "Průchodnost: kola poskytují dobrý záběr na nerovných a kluzkých silnicích.",
+    fatbikeAdvantages2: "Stabilita: Fat bike je pro Prahu lepší, protože jeho široké pneumatiky změkčují dlažební kostky, zvládají sníh a umožňují pohodlnou jízdu po parcích a obrubnících.",
+    fatbikeAdvantages3: "Zima a sníh: Fat bike se snadno pohybuje po sněhu a ledu, což je v zimě důležité.",
+    fatbikeAdvantages4: "Komfort: Fat bike pohlcuje nárazy, snižuje vibrace a únavu kurýra.",
+    fatbikeAdvantages5: "Odolnost: Pevné pneumatiky snižují riziko poškození a počet oprav.",
+    fatbikeAdvantages6: "FATBIKE je tedy pro kurýra pohodlný, zejména za špatného počasí",
+
+    buybackConditionsTitle: "Nabízíme kurýrům výhodné podmínky pro ODKOUPENÍ a KOUPI elektrokol za dostupné ceny. Naše nabídky zahrnují:",
+    buybackConditions1: "Zvýhodněné podmínky nákupu/odkupu – kurýři si mohou pořídit nové elektrokolo s výraznou slevou",
+    buybackConditions2: "Věrnostní program – bonusy a slevy pro kurýry, kteří s námi zůstávají dlouhodobě.",
+    buybackConditions3: "Slevy za objem objednávek – pro kurýry s velkým počtem dokončených objednávek nabízíme další slevy při koupi nebo odkupu elektrokola.",
+    buybackConditions4: "Tyto nabídky pomohou kurýrům pracovat efektivně a úsporně!",
+
+    faqButton: "Často kladené otázky",
+
+    whatWeDoTitle: "Co potřebuji k práci?",
+    whatWeDoAge: "Věk od 16 let",
+    whatWeDoPhone: "Smartphone (iOS, Android) s přístupem k internetu",
+    whatWeDoResidence: "Legální pobyt v České republice",
+
+    whatWeDoCommissionHeader: "Jaké procento si ponecháváte?",
+    whatWeDoCommission: "Bereme 700 Kč z výdělku, který vidíte v aplikaci",
+
+    whatWeDoPayoutHeader: "Kdy posíláte platbu?",
+    whatWeDoPayout1: "Platby se provádějí každé úterý (kromě státních svátků)",
+    whatWeDoPayout2: "Po svátcích do 1-2 pracovních dnů 🤓☝🏼",
+
+    whatWeDoTransferHeader: "Můžete poslat platbu na účet, který mi nepatří?",
+    whatWeDoTransfer: "Ano. Například na účet vašeho příbuzného nebo přítele",
+
+    whatWeDoBenefitsHeader: "Jaké jsou výhody práce kurýra na elektrokole?",
+    whatWeDoBenefits: "Práce kurýra na elektrokole FATBIKE má výhody: -vysoká průchodnost v jakýchkoli podmínkách -komfort na nerovných cestách -rychlost a úspora energie díky elektrickému pohonu -nezávislost na dopravních zácpách a nákladech na palivo 🚴⚡",
+
+    captionHeader: "Pronájem nejlepších elektrokol pro doručení v Praze! 🚴‍♂️🚗",
+
+    contactUsButton: "Kontaktujte nás", 
+    allCategories: "Vše",
+    legendAction: "Co děláme",
+    navCart: "Košík",
+  },
+};
